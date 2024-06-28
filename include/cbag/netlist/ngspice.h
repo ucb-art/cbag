cfdc922f16a842af0281353f204f91aa5e1831f0
Bob Zhou <bob.linchuan@berkeley.edu>
2024-06-27 18:10:41 -0700

    Removing BCAD license from new file

--- include/cbag/netlist/ngspice.h
+++ include/cbag/netlist/ngspice.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BSD-3-Clause AND Apache-2.0
+// SPDX-License-Identifier: BSD-3-Clause
 /*
 Copyright (c) 2018, Regents of the University of California
 All rights reserved.
@@ -27,21 +27,6 @@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-Copyright 2019 Blue Cheetah Analog Design Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 */
 
 #ifndef CBAG_NETLIST_NGSPICE_H
